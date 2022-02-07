47d874078cd1ce20c28be355aab57f77e5142ba0
wensimin <hukaizhiyu2@gmail.com>
2022-02-07 18:46:07 +0800

    coding

--- src/main/kotlin/tech/shali/automission/AutoMissionApplication.kt
+++ src/main/kotlin/tech/shali/automission/AutoMissionApplication.kt
@@ -2,12 +2,15 @@ package tech.shali.automission
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.ClassPathResource
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.net.URLClassLoader
 
 @SpringBootApplication
 @EnableScheduling
 class AutoMissionApplication
 
 fun main(args: Array<String>) {
+    URLClassLoader(arrayOf(ClassPathResource("").url)).loadClass("AutoTaskuuid")
     runApplication<AutoMissionApplication>(*args)
 }
---
+++ src/main/resources/code/Template.kt
@@ -0,0 +1,3 @@
+// "C:\Program Files\JetBrains\IntelliJ IDEA 2020.2\plugins\Kotlin\kotlinc\bin\kotlinc" Test.kt
+class Template {
+}

77b8720379892a2641b8dcd59507806075df7d50
wensimin <hukaizhiyu2@gmail.com>
2022-03-08 19:13:22 +0800

    后端jsr223部分完成

--- src/main/kotlin/tech/shali/automission/entity/Task.kt
+++ src/main/kotlin/tech/shali/automission/entity/Task.kt
@@ -2,12 +2,10 @@ package tech.shali.automission.entity
 
 import org.hibernate.annotations.Type
 import org.hibernate.validator.constraints.Length
-import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Lob
-import javax.validation.constraints.NotEmpty
 
 @Entity
 class Task(

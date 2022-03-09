91b825d5c3fa7b7bbdf0a1953ebc60e0be054896
wensimin <hukaizhiyu2@gmail.com>
2022-03-09 11:06:09 +0800

    删除静态任务

--- src/main/kotlin/tech/shali/automission/dao/TaskParamDao.kt
+++
@@ -1,8 +0,0 @@
-package tech.shali.automission.dao
-
-import org.springframework.data.jpa.repository.JpaRepository
-import tech.shali.automission.entity.TaskParam
-
-interface TaskParamDao : JpaRepository<TaskParam, String> {
-    fun findByKey(accessKey: String):TaskParam
-}
